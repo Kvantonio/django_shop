9a0f769b2fa910eaf9982b3f18433896b76db307
kvantonio <kvantonio@i.ua>
2021-04-04 21:37:00 +0300

    celery start

--- drf_stock/stock/views.py
+++ drf_stock/stock/views.py
@@ -8,16 +8,16 @@ from .serializers import AuthorSerializer, BookSerializer, PublisherSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
 
 
 class PublisherViewSet(viewsets.ModelViewSet):
     queryset = Publisher.objects.all()
     serializer_class = PublisherSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
--- shop/shop/tasks.py
+++ shop/shop/tasks.py
@@ -1,8 +1,21 @@
 from celery import shared_task
+import requests
 
 from django.core.mail import send_mail
+
+from .models import Book
 
 
 @shared_task
 def send_mail_task(subject, message, email):
     send_mail(subject, message, email, ['admin@example.com'])
+
+
+@shared_task
+def input_books():
+    url = 'http://127.0.0.1:8000/stock/books/'
+    response = requests.get(url).json()
+
+
+
+
